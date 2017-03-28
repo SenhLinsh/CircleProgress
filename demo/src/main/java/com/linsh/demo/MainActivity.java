6fec16aa4df79f3a63bdb8ff517a8357a9638954
Senh Linsh <lyuanxiang0812@126.com>
2017-03-28 16:32:51 +0800

    Update demo module for sample.

--- demo/src/main/java/com/linsh/demo/MainActivity.java
+++ demo/src/main/java/com/linsh/demo/MainActivity.java
@@ -1,8 +1,7 @@
 package com.linsh.demo;
 
-import android.graphics.Color;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 import com.linsh.circleprogress.CircleProgress;
@@ -20,48 +19,48 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        int color1 = getResources().getColor(R.color.progressColor1);
+        int color2 = getResources().getColor(R.color.progressColor2);
+        int color3 = getResources().getColor(R.color.progressColor3);
+        int color4 = getResources().getColor(R.color.progressColor4);
+        int[] colors = {color1, color2, color3, color4};
 
         mCircleProgress = (CircleProgress) findViewById(R.id.circleprogress);
-        mCircleProgress.setProgressWidth(20);
-        mCircleProgress.setProgressColor(Color.BLUE);
+        mCircleProgress.setProgressColor(color1);
         mCircleProgress.setOnClickListener(this);
 
         mSeriesCircleProgress = (SeriesCircleProgress) findViewById(R.id.seriescircleprogress);
-        mSeriesCircleProgress.setProgressWidth(20);
-        mSeriesCircleProgress.setProgressColor(new int[]{Color.RED, Color.BLUE, Color.YELLOW});
+        mSeriesCircleProgress.setProgressColor(colors);
         mSeriesCircleProgress.setOnClickListener(this);
 
         mParallelCircleProgress = (ParallelCircleProgress) findViewById(R.id.parallelcircleprogress);
-        mParallelCircleProgress.setProgressWidth(10);
-        mParallelCircleProgress.setProgressColor(new int[]{Color.RED, Color.BLUE, Color.YELLOW});
+        mParallelCircleProgress.setProgressColor(colors);
         mParallelCircleProgress.setOnClickListener(this);
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        onClick(mCircleProgress);
-        onClick(mSeriesCircleProgress);
-        onClick(mParallelCircleProgress);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.circleprogress:
-                mCircleProgress.setProgressWithAnimation(mCircleProgress.isSelected() ? 0 : 60);
+                mCircleProgress.setProgressWithAnimation(mCircleProgress.isSelected() ? 0 : 100);
                 mCircleProgress.setSelected(!mCircleProgress.isSelected());
                 break;
             case R.id.seriescircleprogress:
-                mSeriesCircleProgress.setProgressWithAnimation(mSeriesCircleProgress.isSelected() ? new int[3] : new int[]{40, 30, 20});
+                mSeriesCircleProgress.setProgressWithAnimation(mSeriesCircleProgress.isSelected() ? new int[3] : new int[]{40, 30, 20, 10});
                 mSeriesCircleProgress.setSelected(!mSeriesCircleProgress.isSelected());
                 break;
             case R.id.parallelcircleprogress:
-                mParallelCircleProgress.setProgressWithAnimation(mParallelCircleProgress.isSelected() ? new int[3] : new int[]{100, 50, 20});
+                mParallelCircleProgress.setProgressWithAnimation(mParallelCircleProgress.isSelected() ? new int[3] : new int[]{100, 70, 40, 10});
                 mParallelCircleProgress.setSelected(!mParallelCircleProgress.isSelected());
                 break;
             default:
                 break;
         }
+    }
+
+    public void animating(View view) {
+        onClick(mCircleProgress);
+        onClick(mSeriesCircleProgress);
+        onClick(mParallelCircleProgress);
     }
 }
